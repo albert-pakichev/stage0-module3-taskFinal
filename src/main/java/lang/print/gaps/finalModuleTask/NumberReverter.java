b9991d6ebbc8983b7a0db5932f12d0be21a307cb
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 21:57:52 +0500

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number%10;
+        number /=10;
+        int second = number%10;
+        number /=10;
+        int third = number%10;
+        number /=10;
+        System.out.printf("%d%d%d", first,second,third);
     }
 }
