d99e5ff7b6335f0c2d26e60120ffb2821d968350
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 17:45:19 +0500

    Update PrintingFalse.java

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -4,6 +4,8 @@ public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
+        boolean sravni = first==second;
+        System.out.println(sravni);
         //write code here
     }
 }
