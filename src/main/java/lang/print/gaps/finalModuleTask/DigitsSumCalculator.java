ba38539b58bd8dc36eb7545018107eff11941dba
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 21:50:01 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,22 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        int currentDigit;
+        currentDigit = number%10;
+        sum = sum + currentDigit;
+        number = number/10;
+
+        currentDigit = number%10;
+        sum = sum + currentDigit;
+        number = number/10;
+
+        currentDigit = number%10;
+        sum = sum + currentDigit;
+        number = number/10;
+
+        sum = sum + number;
+
+        System.out.println(sum);
     }
 }
